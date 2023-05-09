de31969a8e09493c91c904d271657b8765f5b199
rafal <rafalgraczykowski737@gmail.com>
2023-05-09 14:17:40 +0200

    Notification mechanism code upgrade

--- app/src/main/java/com/homebudget/service/NotificationService.java
+++ app/src/main/java/com/homebudget/service/NotificationService.java
@@ -3,6 +3,7 @@ package com.homebudget.service;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 
+import com.homebudget.domain.NotificationHandler;
 import com.homebudget.notification.PaymentReminder;
 import com.homebudget.repository.AppDatabase;
 import com.homebudget.repository.PaymentStatus;
@@ -18,43 +19,14 @@ public class NotificationService extends JobService {
 
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
-        showNotification(jobParameters);
+        NotificationHandler.createNotification(getApplicationContext(),this,daysToNotify);
+        jobFinished(jobParameters, false);
         return true;
     }
 
     @Override
     public boolean onStopJob(JobParameters jobParameters) {
         return true;
-    }
-
-    private void showNotification(JobParameters jobParameters) {
-
-        Runnable notify = () -> {
-            LocalDate now = LocalDate.now();
-            String queryTerm = "%" + now.getMonth().getValue() + "-" + now.getYear();
-            List<PaymentView> payments = AppDatabase.getDatabase(getApplicationContext()).paymentDAO().findPaymentViewByTermAndStatus(PaymentStatus.UNPAID.getValue(),queryTerm);
-            StringBuilder message = new StringBuilder();
-            for (PaymentView paymentView : payments) {
-                LocalDate paymentEntityTerm = DateFormatter.formatStringToLocalDate(paymentView.getTerm());
-                if (paymentEntityTerm.getMonth().getValue() == now.getMonth().getValue()) {
-                    for (int i = 0; i <= daysToNotify; i++) {
-                        if (paymentEntityTerm.getDayOfMonth() - i == now.getDayOfMonth()) {
-                            if(message.length() != 0){
-                                message.append(",");
-                            }
-                            message.append(paymentView.getService());
-                            break;
-                        }
-                    }
-                }
-            }
-            if(message.length() != 0){
-                PaymentReminder paymentReminder = new PaymentReminder(this, message.toString());
-                paymentReminder.showNotification();
-            }
-        };
-        AppDatabase.getExecutorService().execute(notify);
-        jobFinished(jobParameters, false);
     }
 
 }
